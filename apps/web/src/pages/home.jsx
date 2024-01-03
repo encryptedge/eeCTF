51d06878358acc097ab18389d3cd8140840be994
JonasSchweigler <swejoz19@htl-kaindorf.at>
2024-01-03 23:09:59 +0100

    added user state in home route

--- apps/web/src/pages/home.jsx
+++ apps/web/src/pages/home.jsx
@@ -1,9 +1,30 @@
 import "../styles/App.css";
 import "react-toastify/dist/ReactToastify.css";
-// import { Link } from "react-router-dom";
+import apiClient from "../libs/api.client";
 import Navbar from "../components/navbar";
+import { useEffect, useState } from "react";
 
 function Home() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+
+    if (!token) return;
+
+    apiClient
+      .get("/user/whoami", {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        if (res.data.error) return;
+
+        if (res.data.message === "Invalid token") return;
+        setUser(res.data);
+      });
+  }, []);
+
   return (
     <div>
       <Navbar pageTitle='Welcome to eeCTF' />
